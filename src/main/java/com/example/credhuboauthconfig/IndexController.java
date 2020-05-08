002902e96be4f769066b8a8326d135a40d98ea42
Yuxin Bai <ybai@pivotal.io>
2020-05-08 15:11:59 -0400

    Reproduce `serverWebExchange cannot be null`

--- src/main/java/com/example/credhuboauthconfig/IndexController.java
+++ src/main/java/com/example/credhuboauthconfig/IndexController.java
@@ -1,11 +1,23 @@
 package com.example.credhuboauthconfig;
 
+import org.springframework.credhub.core.ReactiveCredHubOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class IndexController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
+    public IndexController(ReactiveCredHubOperations credHubOperations) {
+        credHubOperations.credentials().findByPath("/")
+                         .doOnNext(summary -> LOGGER.info("credentials: {}", summary))
+                         .blockFirst();
+    }
 
     @GetMapping("/")
     Mono<String> greeting() {
