a17e3ff6c3e6144e8591e4c391a663c6ab66cf74
Yuxin Bai <ybai@pivotal.io>
2020-05-13 14:28:21 -0400

    Success with credhub 2.0.1

--- src/main/java/com/example/credhuboauthconfig/CredHubOauthConfigApplication.java
+++ src/main/java/com/example/credhuboauthconfig/CredHubOauthConfigApplication.java
@@ -2,12 +2,6 @@ package com.example.credhuboauthconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.web.server.AuthenticatedPrincipalServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 
 @SpringBootApplication
 public class CredHubOauthConfigApplication {
--- src/main/java/com/example/credhuboauthconfig/IndexController.java
+++ src/main/java/com/example/credhuboauthconfig/IndexController.java
@@ -1,26 +1,33 @@
 package com.example.credhuboauthconfig;
 
-import org.springframework.credhub.core.ReactiveCredHubOperations;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+
+import org.springframework.credhub.core.CredHubOperations;
+import org.springframework.credhub.support.CredentialDetails;
+import org.springframework.credhub.support.SimpleCredentialName;
+import org.springframework.credhub.support.value.ValueCredential;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
 
-    public IndexController(ReactiveCredHubOperations credHubOperations) {
-        credHubOperations.credentials().findByPath("/")
-                         .doOnNext(summary -> LOGGER.info("credentials: {}", summary))
-                         .blockFirst();
+    private final CredHubOperations credHubOperations;
+
+    public IndexController(CredHubOperations credHubOperations) {
+        this.credHubOperations = credHubOperations;
     }
 
-    @GetMapping("/")
-    Mono<String> greeting() {
-        return Mono.just("hello");
+    @GetMapping("/read")
+    Mono<String> read() {
+        return Mono.just(
+                credHubOperations
+                        .credentials()
+                        .getByName(new SimpleCredentialName("/c/p.spring-cloud-gateway-service-scg-service-broker/client-certificate"), ValueCredential.class))
+                   .map(CredentialDetails::toString);
     }
 }
